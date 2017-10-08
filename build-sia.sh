2f24d6da7dc7016d520d1f86938f20787928a5e6
Boris Nagaev <bnagaev@gmail.com>
2017-10-09 00:17:43 +0200

    update build-sia.sh

--- build-sia.sh
+++ build-sia.sh
@@ -7,8 +7,8 @@ T2=$(mktemp -d)
 wget -O "${T2}/go1.4-bootstrap-20170531.tar.gz" "https://storage.googleapis.com/golang/go1.4-bootstrap-20170531.tar.gz"
 echo "49f806f66762077861b7de7081f586995940772d29d4c45068c134441a743fa2  ${T2}/go1.4-bootstrap-20170531.tar.gz" | sha256sum --check --strict -
 tar -C "${T2}" -xzf "${T2}/go1.4-bootstrap-20170531.tar.gz"
-if [ -f "${T2}/go/include/libc.h" ]; then sed "s/struct timespec {/struct timespec_disabled_by_gohere {/g" -i "${T2}/go/include/libc.h"; fi
-find "${T2}/go" -name "*.c" -print0 | xargs -0 -I cfile sed "s/(vlong)~0 << 32/(uvlong)~0 << 32/g" -i cfile
+sed -i.bak -e "s/struct timespec {/struct timespec_disabled_by_gohere {/g" -- "${T2}/go/include/libc.h"
+find "${T2}/go" -name "*.c" -print0 | xargs -0 -I cfile sed -i.bak -e "s/(vlong)~0 << 32/(uvlong)~0 << 32/g" -- cfile
 cd "${T2}/go/src" && GOROOT_FINAL="${T1}/go1.4-bootstrap-20170531_bootstrap" GOROOT_BOOTSTRAP="" ./make.bash | grep "Installed Go"
 mkdir -p "${T1}/go1.4-bootstrap-20170531_bootstrap"
 cp -a "${T2}/go/src" "${T2}/go/bin" "${T2}/go/pkg" "${T2}/go/misc" "${T2}/go/include" "${T1}/go1.4-bootstrap-20170531_bootstrap"
@@ -16,8 +16,7 @@ rm -rf "${T2}"
 wget -O "${T1}/go1.9.1.src.tar.gz" "https://storage.googleapis.com/golang/go1.9.1.src.tar.gz"
 echo "a84afc9dc7d64fe0fa84d4d735e2ece23831a22117b50dafc75c1484f1cb550e  ${T1}/go1.9.1.src.tar.gz" | sha256sum --check --strict -
 tar -C "${T1}" -xzf "${T1}/go1.9.1.src.tar.gz"
-if [ -f "${T1}/go/include/libc.h" ]; then sed "s/struct timespec {/struct timespec_disabled_by_gohere {/g" -i "${T1}/go/include/libc.h"; fi
-find "${T1}/go" -name "*.c" -print0 | xargs -0 -I cfile sed "s/(vlong)~0 << 32/(uvlong)~0 << 32/g" -i cfile
+find "${T1}/go" -name "*.c" -print0 | xargs -0 -I cfile sed -i.bak -e "s/(vlong)~0 << 32/(uvlong)~0 << 32/g" -- cfile
 cd "${T1}/go/src" && GOROOT_FINAL="/tmp/sia-build/goroot" GOROOT_BOOTSTRAP="${T1}/go1.4-bootstrap-20170531_bootstrap" ./make.bash | grep "Installed Go"
 mkdir -p "/tmp/sia-build/goroot"
 cp -a "${T1}/go/src" "${T1}/go/bin" "${T1}/go/pkg" "${T1}/go/misc" "/tmp/sia-build/goroot"
