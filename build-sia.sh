0da4f8f41db86fd1736baafbc8c178d1fed13592
Boris Nagaev <bnagaev@gmail.com>
2017-10-09 21:39:51 +0200

    no mktemp

--- build-sia.sh
+++ build-sia.sh
@@ -2,8 +2,10 @@
 # Dependencies: bash coreutils wget tar sed gcc make
 set -xue
 rm -rf '/tmp/sia-build'
-T1=$(mktemp -d)
-T2=$(mktemp -d)
+T1="/tmp/sia-build/goroot/T1"
+mkdir -p "$T1"
+T2="${T1}/go1.4-bootstrap-20170531_bootstrap/T2"
+mkdir -p "$T2"
 wget -O "${T2}/go1.4-bootstrap-20170531.tar.gz" "https://storage.googleapis.com/golang/go1.4-bootstrap-20170531.tar.gz"
 echo "49f806f66762077861b7de7081f586995940772d29d4c45068c134441a743fa2  ${T2}/go1.4-bootstrap-20170531.tar.gz" | sha256sum --check --strict -
 tar -C "${T2}" -xzf "${T2}/go1.4-bootstrap-20170531.tar.gz"
@@ -12,7 +14,7 @@ find "${T2}/go" -name "*.c" -print0 | xargs -0 -I cfile sed -i.bak -e "s/(vlong)
 cd "${T2}/go/src" && GOROOT_FINAL="${T1}/go1.4-bootstrap-20170531_bootstrap" GOROOT_BOOTSTRAP="" ./make.bash | grep "Installed Go"
 mkdir -p "${T1}/go1.4-bootstrap-20170531_bootstrap"
 cp -a "${T2}/go/src" "${T2}/go/bin" "${T2}/go/pkg" "${T2}/go/misc" "${T2}/go/include" "${T1}/go1.4-bootstrap-20170531_bootstrap"
-rm -rf "${T2}"
+rm -rf "$T2"
 wget -O "${T1}/go1.9.1.src.tar.gz" "https://storage.googleapis.com/golang/go1.9.1.src.tar.gz"
 echo "a84afc9dc7d64fe0fa84d4d735e2ece23831a22117b50dafc75c1484f1cb550e  ${T1}/go1.9.1.src.tar.gz" | sha256sum --check --strict -
 tar -C "${T1}" -xzf "${T1}/go1.9.1.src.tar.gz"
@@ -20,7 +22,7 @@ find "${T1}/go" -name "*.c" -print0 | xargs -0 -I cfile sed -i.bak -e "s/(vlong)
 cd "${T1}/go/src" && GOROOT_FINAL="/tmp/sia-build/goroot" GOROOT_BOOTSTRAP="${T1}/go1.4-bootstrap-20170531_bootstrap" ./make.bash | grep "Installed Go"
 mkdir -p "/tmp/sia-build/goroot"
 cp -a "${T1}/go/src" "${T1}/go/bin" "${T1}/go/pkg" "${T1}/go/misc" "/tmp/sia-build/goroot"
-rm -rf "${T1}"
+rm -rf "$T1"
 
 export PATH="/tmp/sia-build/goroot/bin:${PATH}"
 export GOPATH='/tmp/sia-build/gopath'
